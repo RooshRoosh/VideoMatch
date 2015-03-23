417524855197293902e807199059a4f235eea8d7
Руслан <newroosh90@gmail.com>
2015-03-23 17:18:08 +0500

    файл в контекстный менеджер

--- application.py
+++ application.py
@@ -13,6 +13,5 @@ def main(russian_question):
         yield from youtube_search(search_query=item)
 
 if __name__ == '__main__':
-    f = open('out.json', 'w')
-    json.dump([result for result in main('Как сделать красиво?')], f)
-    f.close()
+    with open('out.json', 'w') as f:
+        json.dump([result for result in main('Как сделать красиво?')], f)
